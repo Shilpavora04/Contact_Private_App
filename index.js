eb3de58164bf024c7de4bca21da7200cb6ebd8be
Shilpavora04 <shilpa.thoughtmate@gmail.com>
2025-12-03 14:47:23 +0530

    Update rcord

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.set("view engine", "pug");
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 app.get("/contacts", async (req, res) => {
- const contacts = "https://api.hubspot.com/crm/v3/objects/contacts";
+ const contacts =
+  "https://api.hubspot.com/crm/v3/objects/contacts?properties=firstname,lastname,email,favorite_book";
+
  const headers = {
   Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
   "Content-Type": "application/json",
@@ -24,6 +29,52 @@ app.get("/contacts", async (req, res) => {
   res.render("contacts", { title: "Contacts | HubSpot APIs", data });
  } catch (error) {
   console.error(error);
+ }
+});
+
+app.get("/update", async (req, res) => {
+ // http://localhost:3000/update?email=rick@crowbars.net
+ const email = req.query.email;
+
+ const getContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email&properties=email,favorite_book`;
+ const headers = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  "Content-Type": "application/json",
+ };
+
+ try {
+  const response = await axios.get(getContact, { headers });
+  const data = response.data;
+
+  // res.json(data);
+  res.render("update", {
+   userEmail: data.properties.email,
+   favoriteBook: data.properties.favorite_book,
+  });
+ } catch (err) {
+  console.error(err);
+ }
+});
+
+app.post("/update", async (req, res) => {
+ const update = {
+  properties: {
+   favorite_book: req.body.newVal,
+  },
+ };
+
+ const email = req.query.email;
+ const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+ const headers = {
+  Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+  "Content-Type": "application/json",
+ };
+
+ try {
+  await axios.patch(updateContact, update, { headers });
+  res.redirect("back");
+ } catch (err) {
+  console.error(err);
  }
 });
 
